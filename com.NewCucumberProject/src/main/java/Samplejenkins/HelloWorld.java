88f4a3fe0da5f7c7c0b7380614402a2a8ea1aac3
New <New@JAIPRASHANTH>
2020-06-25 20:15:42 +0530

    7th commit from eclipse

--- com.NewCucumberProject/src/main/java/Samplejenkins/HelloWorld.java
+++ com.NewCucumberProject/src/main/java/Samplejenkins/HelloWorld.java
@@ -1,8 +1,16 @@
 package Samplejenkins;
 
-public class HelloWorld {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class HelloWorld {
+static WebDriver driver;
 	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver","D:\\All selenium projects\\ALL selenium projects\\com.Cucumber.project\\Drivers\\chromedriver.exe");
+		driver=new ChromeDriver();
+		
+		driver.get("https://admin-demo.nopcommerce.com/login?ReturnUrl=%2Fadmin%2F");
 		
 System.out.println("helloworld");
 System.out.println("helloworld2");
